3fe7a4dc02ae82f5566a04ede8186dbd6a165f0d
elizabeth :) <106013418+elegantquokka@users.noreply.github.com>
2022-06-05 11:58:11 +1000

    initialised repository

---
+++ embeds.js
@@ -0,0 +1,6 @@
+const { MessageEmbed } = require('discord.js');
+const errorEmbed = new MessageEmbed()
+    .setColor('#ff0000')
+    .setTitle('Tragically, GreenBean is unsatisfied with your input ~~or possibly dead~~.');
+
+module.exports = errorEmbed;
---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const { errorEmbed } = require('./embeds.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log(`GreenBean ready! Logged in as ${client.user.tag}`);
+});
+
+client.commands = new Collection();
+const commandFiles = [];
+for (const file of commandFiles) {
+  const command = require(`./${file}`);
+  client.commands.set(command.data.name, command);
+}
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  }
+  catch (error) {
+    console.error(error);
+    await interaction.reply({ embeds: [errorEmbed] });
+  }
+
+});
+
+client.login(token);
